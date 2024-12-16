dd5455e18314c2847dddb8e73f68b82acac5f990
Beno <chocomilo451@gmail.com>
2024-12-16 16:32:45 +0000

    Completed the first three custom enumerable methods

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,36 @@
 module Enumerable
-  # Your code goes here
+  def my_each
+    return to_enum(:my_each) unless block_given?
+
+    i = 0
+    while i < length
+      yield(self[i])
+      i += 1
+    end
+    self
+  end
+
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
+    i = 0
+    while i < length
+      yield(self[i], i)
+      i += 1
+    end
+    self
+  end
+
+  def my_select
+    return to_enum(:my_select) unless block_given?
+
+    new_array = []
+    my_each { |i| new_array << i if yield(i) }
+    new_array
+  end
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
+# Extend the Array class to include the Enumerable module
 class Array
-  # Define my_each here
+  include Enumerable
 end
